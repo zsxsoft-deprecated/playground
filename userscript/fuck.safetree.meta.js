9b7907b2589b8e8afb23aa8db2b9f2ed08357acc
zsx <zsx@zsxsoft.com>
2015-07-10 11:27:22 +0800

    Submit userscript

---
+++ userscript/fuck.safetree.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         自动刷题 - Fuck 2015 SafeTree
+// @namespace    http://2015sj.safetree.com.cn/
+// @version      0.1
+// @description  Fuck 2015 SafeTree
+// @author       zsx
+// @include      http://2015sj.safetree.com.cn/*
+// @grant        none
+// @updateURL   http://zsxsoft.github.io/playground/userscript/fuck.safetree.meta.js
+// @downloadURL http://zsxsoft.github.io/playground/userscript/fuck.safetree.user.js
+// ==/UserScript==
