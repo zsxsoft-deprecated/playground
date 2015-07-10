2319da89a93173e8486a02a389823b1ef2ff0300
zsx <zsx@zsxsoft.com>
2015-07-10 11:23:42 +0800

    修改执行步骤

--- userscript/fuck.safetree.js
+++ userscript/fuck.safetree.js
@@ -10,30 +10,45 @@
 // ==/UserScript==
 
 (function() {
-    var login = function login(username, password) {
-        $.getJSON(RegisterUrl + "/SpecialLoginHandler.asmx/SpecialLogin?jsoncallback=?", { r: Math.random(), account: username, password: password }, function (json) {
-            InsertFnishWorkOne();
-            SubmitTestInfo("1|1|1|0|0|3|2|3|3|2|7|2|1|3|5|5|1|1|4|5|2|0|0|0|0|0|0", 3, 100);
-            SubmitFireCheck("1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1");
+    var login = function login(username, password, callback) {
+        $.getJSON(RegisterUrl + "/SpecialLoginHandler.asmx/SpecialLogin?jsoncallback=?", {
+            r: Math.random(),
+            account: username,
+            password: password
+        }, function(json) {
+            InsertFnishWorkOne(function() {
+                SubmitTestInfo("1|1|1|0|0|3|2|3|3|2|7|2|1|3|5|5|1|1|4|5|2|0|0|0|0|0|0", 3, 100, function() {
+                    SubmitFireCheck("1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1", function() {
+                        delCookie("2_qm1");
+                        delCookie("2_qm2");
+                        delCookie("2_qm3");
+                        delCookie("InsertID");
+                        $.getJSON(Asmx + "/LoginOut?jsoncallback=?", {
+                            r: Math.random()
+                        }, function() {
+                            alert("刷题完成：" + username);
+                            callback();
+                        });
+                    });
+                });
+            });
+
+
         });
-        delCookie("2_qm1");
-        delCookie("2_qm2");
-        delCookie("2_qm3");
-        delCookie("InsertID");
-        $.getJSON(Asmx + "/LoginOut?jsoncallback=?", { r: Math.random() }, function () {
-             alert("刷题完成：" + username);
-        });
+
         //special.userExit();
     }
     if (!confirm("开启刷题？")) return false;
 
-   
-    while (true) {
-        
+
+    var reLogin = function() {
+
         var userName = prompt("请输入用户名");
         if (userName == "") return;
         var userPassword = prompt("请输入密码");
-        login(userName, userPassword);
+        login(userName, userPassword, reLogin);
 
     }
+
+    reLogin();
 })();
