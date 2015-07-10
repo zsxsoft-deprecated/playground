8c4425b6874e8f13913f251139cf18e567b5b9d5
zsx <zsx@zsxsoft.com>
2015-07-10 11:17:23 +0800

    自动刷题 - Fuck 2015 SafeTree

---
+++ userscript/fuck.safetree.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         自动刷题 - Fuck 2015 SafeTree
+// @namespace    http://2015sj.safetree.com.cn/
+// @version      0.1
+// @description  Fuck 2015 SafeTree
+// @author       zsx
+// @include      http://2015sj.safetree.com.cn/*
+// @require      http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var login = function login(username, password) {
+        $.getJSON(RegisterUrl + "/SpecialLoginHandler.asmx/SpecialLogin?jsoncallback=?", { r: Math.random(), account: username, password: password }, function (json) {
+            InsertFnishWorkOne();
+            SubmitTestInfo("1|1|1|0|0|3|2|3|3|2|7|2|1|3|5|5|1|1|4|5|2|0|0|0|0|0|0", 3, 100);
+            SubmitFireCheck("1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1");
+        });
+        delCookie("2_qm1");
+        delCookie("2_qm2");
+        delCookie("2_qm3");
+        delCookie("InsertID");
+        $.getJSON(Asmx + "/LoginOut?jsoncallback=?", { r: Math.random() }, function () {
+             alert("刷题完成：" + username);
+        });
+        //special.userExit();
+    }
+    if (!confirm("开启刷题？")) return false;
+
+   
+    while (true) {
+        
+        var userName = prompt("请输入用户名");
+        if (userName == "") return;
+        var userPassword = prompt("请输入密码");
+        login(userName, userPassword);
+
+    }
+})();
